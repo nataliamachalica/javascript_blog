30dd6a31bcd39de2a7e12c9fff8117b2de0ccdf3
Natalia Szlapka <n.machalica@gmail.com>
2021-01-24 20:11:55 +0100

    Add class active to clicked links

--- js/script.js
+++ js/script.js
@@ -6,14 +6,31 @@
   });*/
 
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log('event');
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+        for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
     /* add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    clickedElement.classList.add('active');
+    console.log('clickedElement (with plus): ', clickedElement);
+
+    /* [DONE] remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('article.active');
+
+        for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+
 
     /* get 'href' attribute from the clicked link */
 
@@ -23,8 +40,8 @@
 
     }
   
-  const links = document.querySelectorAll('.titles a');
+    const links = document.querySelectorAll('.titles a');
 
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+        for(let link of links){
+        link.addEventListener('click', titleClickHandler);
+    }
