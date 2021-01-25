802eac6916f0fad5e3d7d183eef0a2ac63f4ae37
Natalia Szlapka <n.machalica@gmail.com>
2021-01-25 14:19:15 +0100

    Remove contents of title list

--- js/script.js
+++ js/script.js
@@ -46,9 +46,37 @@
     targetArticle.classList.add('active');
 
     }
-  
+
+    const optArticleSelector = '.post',
+        optTitleSelector = '.post-title',
+        optTitleListSelector = '.titles';
+
+    function generateTitleLinks(){
+
+    /* [DONE] remove contents of titleList */
+
+    const titleList = document.querySelector(optTitleListSelector);
+        titleList.innerHTML = '';
+
+    /* for each article */
+
+    
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+    }
     const links = document.querySelectorAll('.titles a');
 
-        for(let link of links){
-        link.addEventListener('click', titleClickHandler);
+    for(let link of links){
+    link.addEventListener('click', titleClickHandler);
     }
+
+    generateTitleLinks();
